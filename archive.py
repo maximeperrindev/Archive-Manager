a2591e5797b4d9cc6c339fe8be9c2623735c8bd5
Maxime Perrin <maxime.perrin@telecom-st-etienne.fr>
2021-12-21 16:04:10 +0100

    Webhook mattermost

--- archive.py
+++ archive.py
@@ -13,7 +13,16 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
+import requests
 
+## SUCCESS VARIABLE
+confSuccess = False
+downloadZipSuccess = False
+zipExtractedSuccess = False
+diffDateSuccess = False
+sentToServerSuccess = False
+checkOutdated = False
+success = False
 
 def send_success_mail(receiver):
     try:
@@ -99,6 +108,7 @@ def hash_zip(zipfile):
     return sha1.hexdigest()
 
 def dowloadFileFromUrl(url):
+    global downloadZipSuccess
     file_name = url.split('/')[-1]
     u = urllib.request.urlopen(url)
     f = open(file_name, 'wb')
@@ -121,19 +131,24 @@ def dowloadFileFromUrl(url):
 
     if prct[1] == 100.00:
         logger.info(file_name+ "("+ str(file_size)+" bytes) "+ "downloaded with success")
+        downloadZipSuccess = True
     else:
         logger.error("Can't download " + file_name+" (", str(file_size)+ " bytes)")
+        downloadZipSuccess = False
     f.close()
     return file_name
 
-## SUCCESS VARIABLE
-success = False
 ## GET CONFIG
-config = configparser.ConfigParser()
-config.read('conf.ini')
-file_conf = config['FILE']
-server_conf = config['SERVER']
-mail_conf = config['MAIL']
+try:
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+    file_conf = config['FILE']
+    server_conf = config['SERVER']
+    mail_conf = config['MAIL']
+    mattermost_conf = config['MATTERMOST']
+    confSuccess = True
+except:
+    confSuccess = False
 ## INSTANCE LOG FILE
 logger = logging.getLogger("archive")   # > set up a new name for a new logger
 
@@ -150,95 +165,147 @@ logger.addHandler(log_handler)
 logger.info("Starting script...")
 
 ## DOWNLOAD FILE FROM SERVER
-r = requests.get(file_conf['URL'], allow_redirects=True)
+r = requests.get(file_conf['URL'])
 if r.status_code != 404:
     print('Resource found ! Downloading...')
     file_name = dowloadFileFromUrl(file_conf['URL'])
-else:
-    logger.error(file_conf['URL'], " not found (HTTP ERROR 404)")
+    ## UNZIP DOWLOADED ZIP AND EXTRACT TO /resources
+    with zipfile.ZipFile(file_name, 'r') as zip_ref:
+        listOfFileNames = zip_ref.namelist()
+        nbDownload = 0
+        same = False
+        for fileName in listOfFileNames:
+            print(fileName)
+            if fileName == file_conf['SQLFILE']:
+                # Extract a single file from zip
+                    hash = hash_zip(zip_ref.open(fileName))
+                    if os.path.isfile("./resources/"+file_conf['SQLFILE']):
+                        if hash == hash_file("./resources/"+file_conf['SQLFILE']):
+                            same = True
+                            logger.error(file_conf['SQLFILE']+ " same as yesterday (hash: "+hash+")")
+                        else:
+                            zip_ref.extract(fileName, "./resources")
+                            nbDownload+=1
+                            diffDateSuccess = True
+                    else:
+                        zip_ref.extract(fileName, "./resources")
+                        nbDownload+=1
+                        diffDateSuccess = True
 
-## UNZIP DOWLOADED ZIP AND EXTRACT TO /resources
-with zipfile.ZipFile(file_name, 'r') as zip_ref:
-   listOfFileNames = zip_ref.namelist()
-   nbDownload = 0
-   same = False
-   for fileName in listOfFileNames:
-       print(fileName)
-       if fileName == file_conf['SQLFILE']:
-           # Extract a single file from zip
-            hash = hash_zip(zip_ref.open(fileName))
-            if os.path.isfile("./resources/"+file_conf['SQLFILE']):
-                if hash == hash_file("./resources/"+file_conf['SQLFILE']):
-                    same = True
-                    logger.error(file_conf['SQLFILE']+ " same as yesterday (hash: "+hash+")")
-                else:
-                    zip_ref.extract(fileName, "./resources")
-                    nbDownload+=1
-            else:
-                zip_ref.extract(fileName, "./resources")
-                nbDownload+=1
-if nbDownload == 0:
-    logger.error(file_conf['SQLFILE']+ " not extracted ! Aborting...")
-else:
-    logger.info(file_conf['SQLFILE']+ ' extracted with success in /resources/'+fileName)
+    if nbDownload == 0:
+        logger.error(file_conf['SQLFILE']+ " not extracted ! Aborting...")
+        zipExtractedSuccess = False
+    else:
+        logger.info(file_conf['SQLFILE']+ ' extracted with success in /resources/'+fileName)
+        zipExtractedSuccess = True
 
-## CONNECTION TO A REMOTE SERVER (WEBDAV)
-options = {
-'webdav_hostname': server_conf['URL'],
-'webdav_login':    server_conf['USERNAME'],
-'webdav_password': server_conf['PASSWORD']
-}
-try:
-    logger.info("Connecting to remote server...")
-    client = Client(options)
-    client.verify = False # To not check SSL certificates (Default = True)
-    client.mkdir('archive')
-    logger.info("Connected !")
-    ## CHECK CONSERVATION TIME FOR .tgz FILES
-    expiration = datetime.today() - timedelta(days=int(server_conf['CONSERVATION_TIME']))
+    ## CONNECTION TO A REMOTE SERVER (WEBDAV)
+    options = {
+    'webdav_hostname': server_conf['URL'],
+    'webdav_login':    server_conf['USERNAME'],
+    'webdav_password': server_conf['PASSWORD']
+    }
     try:
-        logger.info('Checking for expired files...')
-        files = client.list("archive", get_info=True)
-        for file in files:
-            if file['content_type'] == 'application/x-gzip':
-                date_str = file['path'].split('/')[3].split('.')[0]
-                date_file = datetime.strptime(date_str, '%Y%d%m')
-                if date_file < expiration:
-                    path = file["path"].split('/')[0] + "/"+file["path"].split('/')[1]
-                    print(path)
-                    client.clean(path)
-                    logger.info(date_str+".tgz too old ! Deleting...")
-        logger.info('Folder is up to date !')
-        success = True
-    except:
-        logger.error("Error while checking for expired files")
-
-    ## CREATE .tgz FILE AND UPLOAD
-    if not same:
-        ## CREATE AAAADDMM.tgz FILE
-        today = datetime.today().strftime('%Y%d%m')
-        new_filename = today+".tgz"
-        make_tarfile("./resources/"+new_filename, "./resources/"+file_conf['SQLFILE'])
-        logger.info(new_filename+" file created in ./resources")
-        print(new_filename+" file created in ./resources")
-
-        ## UPLOAD .tgz ON SERVER
+        logger.info("Connecting to remote server...")
+        client = Client(options)
+        client.verify = False # To not check SSL certificates (Default = True)
+        client.mkdir('archive')
+        logger.info("Connected !")
+        ## CHECK CONSERVATION TIME FOR .tgz FILES
+        expiration = datetime.today() - timedelta(days=int(server_conf['CONSERVATION_TIME']))
         try:
-            client.upload_sync(remote_path="archive/"+new_filename, local_path="./resources/"+new_filename)
-            logger.info("Uploaded !")
+            logger.info('Checking for expired files...')
+            files = client.list("archive", get_info=True)
+            for file in files:
+                if file['content_type'] == 'application/x-gzip':
+                    date_str = file['path'].split('/')[3].split('.')[0]
+                    date_file = datetime.strptime(date_str, '%Y%d%m')
+                    if date_file < expiration:
+                        path = file["path"].split('/')[2] + "/"+file["path"].split('/')[3]
+                        client.clean(path)
+                        logger.info(date_str+".tgz too old ! Deleting...")
+            logger.info('Folder is up to date !')
+            checkOutdated = True
             success = True
         except:
-            success = False
-            logger.error("Error while uploading data to server")
-except:
-    logger.error("Error while connecting to server : " + server_conf['URL'])
+            logger.error("Error while checking for expired files")
+            checkOutdated = False
+
+        ## CREATE .tgz FILE AND UPLOAD
+        if not same:
+            ## CREATE AAAADDMM.tgz FILE
+            today = datetime.today().strftime('%Y%d%m')
+            new_filename = today+".tgz"
+            make_tarfile("./resources/"+new_filename, "./resources/"+file_conf['SQLFILE'])
+            logger.info(new_filename+" file created in ./resources")
+            print(new_filename+" file created in ./resources")
+
+            ## UPLOAD .tgz ON SERVER
+            try:
+                client.upload_sync(remote_path="archive/"+new_filename, local_path="./resources/"+new_filename)
+                logger.info("Uploaded !")
+                success = True
+                sentToServerSuccess = True
+            except:
+                success = False
+                logger.error("Error while uploading data to server")
+    except:
+        logger.error("Error while connecting to server : " + server_conf['URL'])
+else:
+    logger.error(file_conf['URL'] + " not found (HTTP ERROR 404)")
+    success = False
+
 
 ## SENDING MAILS
 mail_list = mail_conf['MAIL_LIST'].split(' ')
 if success:
     for mail in mail_list:
-        send_success_mail(mail)
-
+       send_success_mail(mail)
 else:
     for mail in mail_list:
-        send_error_mail(mail)
+       send_error_mail(mail)
+
+## SEND MATTERMOST NOTIFICATION
+today = datetime.today().strftime('%d %B, %Y')
+subject = "#### Archiving report for "+ today+"\nPlease, check logs if some errors occured.\n"
+if confSuccess:
+    confSuccessMessage = ":white_check_mark: |"
+else:
+    confSuccessMessage = ":warning:          |"
+
+if downloadZipSuccess:
+    downloadSuccessMessage = ":white_check_mark: |"
+else:
+    downloadSuccessMessage = ":warning:          |"
+
+if zipExtractedSuccess:
+    extractedSuccessMessage = ":white_check_mark: |"
+else:
+    extractedSuccessMessage = ":warning:          |"
+
+if diffDateSuccess:
+    diffSuccessMessage = ":white_check_mark: |"
+else:
+    diffSuccessMessage = ":warning:          |"
+
+if sentToServerSuccess:
+    sentSuccessMessage = ":white_check_mark: |"
+else:
+    sentSuccessMessage = ":warning:          |"
+if checkOutdated:
+    outdatedMessage = ":white_check_mark: |"
+else:
+    outdatedMessage = ":warning:          |"
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+
+table ="| Task             | State              |\n"+"|:-----------------|:-------------------|\n"+"| Get config data  | "+ confSuccessMessage +"\n" +"| Download zip     | "+ downloadSuccessMessage+"\n" +"| Zip extracted    | "+ extractedSuccessMessage+"\n" +"| Different file   | "+ diffSuccessMessage+"\n"+"| Check for outdated files   | "+ outdatedMessage+"\n" +"| Archive uploaded | "+sentSuccessMessage
+
+data = {"username": "Archive Manager","icon_url": "https://mattermost.org/wp-content/uploads/2016/04/icon.png","text":  subject + "\n" + table}
+
+try:
+    x = requests.post(mattermost_conf['WEBHOOK'],data = json.dumps(data), headers = headers)
+    logger.info("Mattermost webhook notification done ! (" + mattermost_conf['WEBHOOK'] + ")")
+except:
+    logger.error("Mattermost webhook notification failed ! Check the url or you might exceeded the allowed rate of requests. (" + mattermost_conf['WEBHOOK']+ ")")
